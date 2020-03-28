ec890145e30e54c6a744deb21f01c18b52b73e43
Tom Jouvet <tom.jouvet@live.fr>
2020-03-29 00:33:28 +0100

    Update TabWidget.js

--- argon-dashboard-react-master/src/components/TabWidget/TabWidget.js
+++ argon-dashboard-react-master/src/components/TabWidget/TabWidget.js
@@ -332,7 +332,7 @@ export default function TabWidget() {
           console.log(error.message);
           //return "ok4";
         }
-            });
+      });
     };
 
     function PatientProfile(pacient)
