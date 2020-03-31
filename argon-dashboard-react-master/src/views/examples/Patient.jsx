c7784faa9cac328fdc05b04367731cfba7bbacaf
Tom Jouvet <tom.jouvet@live.fr>
2020-03-31 08:41:04 +0100

    Update Patient.jsx

--- argon-dashboard-react-master/src/views/examples/Patient.jsx
+++ argon-dashboard-react-master/src/views/examples/Patient.jsx
@@ -68,288 +68,288 @@ import MeasuresWidget from "components/TabWidget/MeasuresWidget";
 import sendMail from "../../components/Mail.js"
 
 export default function Patient (props){
-    const history = useHistory();
-    var info = JSON.parse(localStorage.getItem("CURRENT_PATIENT"));
-    const [value, setValue] = useState(-1);
-    const [show5Graphs, setShow5Graphs] = useState(false);
-    console.log("The value is")
-    console.log(value)
-    let infoPodiatrist= JSON.parse(localStorage.getItem("CURRENT_USER"));
+  const history = useHistory();
+  var info = JSON.parse(localStorage.getItem("CURRENT_PATIENT"));
+  const [value, setValue] = useState(-1);
+  const [show5Graphs, setShow5Graphs] = useState(false);
+  console.log("The value is")
+  console.log(value)
+  let infoPodiatrist= JSON.parse(localStorage.getItem("CURRENT_USER"));
 
-    let emailMessage = `Hello ${info.name} ${info.lastname}, your podiatrist ${infoPodiatrist.name} ${infoPodiatrist.lastname} that you last saw on ${info.lastMeetingDate? info.lastMeetingDate: "None"} think that you need to schedule a appointment with him. \n Have a good day, \n\n Feetback application.`
+  let emailMessage = `Hello ${info.name} ${info.lastname}, your podiatrist ${infoPodiatrist.name} ${infoPodiatrist.lastname} that you last saw on ${info.lastMeetingDate? info.lastMeetingDate: "None"} think that you need to schedule a appointment with him. \n Have a good day, \n\n Feetback application.`
 
-    if(props.location.state==null)
-        return(
-        <>
-        <Header />
-        {/* Page content */}
-        <Container className="mt--7" fluid>
-        <Row className="mt-5">
-        <Col className="mb-5 mb-xl-0" xl="6"></Col>
-          <p>Wrong access, return to the patients list</p> 
-        </Row>
-        </Container>
-        
-        </>);
-    return (
+  if(props.location.state==null)
+    return(
       <>
       <Header />
     {/* Page content */}
     <Container className="mt--7" fluid>
-     <Row className ="text-right">
-       <Col></Col>
-       <Col></Col>
-       <Col>
-<Anomaly val={info.anomaly}></Anomaly></Col> 
-       <Col className="text-right">
-
-         
-       <Button
-                      color="default"
-                      onClick={() => sendMail(info.email, infoPodiatrist.email, "Schedule a meeting soon!", emailMessage)}
-                      size="sm"
-                    >
-      Schedule a meeting 
-      </Button>
-
-       <Button
-    onClick={() => setShow5Graphs(!show5Graphs)}
-    >
-      {(show5Graphs)? "Show main graph": "Show 5 sensors graphs" }
-    </Button>
-       </Col>
-  <Col><Button
-href="#pablo"
-onClick={e => e.preventDefault()}
->
-Schedule a meeting
-</Button></Col>
-       </Row> 
-
     <Row className="mt-5">
-    
-    {(show5Graphs)?  
-    <>
-    <Col className="mb-5 mb-xl-0" xl="6">
-              <Card className="bg-gradient-default shadow">
-
-                <CardHeader className="bg-transparent">
-
-                  <Row className="align-items-center">
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">
-                        Sensor 1
-                      </h6>
-                      <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>
-                    <div className="col">
-                    </div>
-                  </Row>
-
-                </CardHeader>
-
-                <CardBody>
-                  {/* Chart */}
-                  
-                  <Graph sensorNumber={0} pacient={ props.location.state.pacient} ></Graph>
-
-                </CardBody>
-
-              </Card>
-
-            </Col>
-
-            <Col className="mb-5 mb-xl-0" xl="6">
-              <Card className="bg-gradient-default shadow">
-                <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">
-                        Sensor 2
-                      </h6>
-                      <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>
-                    
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  {/* Chart */}
-                  
-                  <Graph sensorNumber={1} pacient={ props.location.state.pacient} ></Graph>
-
-                </CardBody>
-              </Card>
-            </Col>
-
-            <Col className="mb-5 mb-xl-0" xl="6">
-              <Card className="bg-gradient-default shadow">
-                <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
-
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">
-                        Sensor 3
-                      </h6>
-                      <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>
-
-                  </Row>
-
-                </CardHeader>
-
-                <CardBody>
-                  {/* Chart */}
-
-                  <Graph sensorNumber={2} pacient={ props.location.state.pacient} ></Graph>
-
-                </CardBody>
-
-              </Card>
-            </Col>  
-
-
-            <Col className="mb-5 mb-xl-0" xl="6">
-              <Card className="bg-gradient-default shadow">
-                <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">
-                        Sensor 4
-                      </h6>
-                      <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>                    
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  {/* Chart */}
-
-                  <Graph sensorNumber={3} pacient={ props.location.state.pacient} ></Graph>
-
-                </CardBody>
-              </Card>
-            </Col>
-
-
-            <Col className="mb-5 mb-xl-0" xl="6">
-              <Card className="bg-gradient-default shadow">
-                <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">
-                        Sensor 5
-                      </h6>
-                      <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  {/* Chart */}
-
-                  <Graph sensorNumber={4} pacient={ props.location.state.pacient}></Graph>
-
-                </CardBody>
-              </Card>
-            </Col>
-           </> 
-            :
-    <>
-    <Col className="mb-5 mb-xl-0" xl="12">
-              <Card className="bg-gradient-default shadow">
-                <CardHeader className="bg-transparent">
-                  <Row className="align-items-center">
-                    <div className="col">
-                      <h6 className="text-uppercase text-light ls-1 mb-1">{
-                      (value== -1) ? 
-                        `All sensors `
-                        : `Sensor ${value +1}`
-                        }                      </h6>
-    <h2 className="text-white mb-0">Pressure values</h2>
-                    </div>
-                    <UncontrolledDropdown nav>
-                <DropdownToggle className="pr-0" nav>
-                  
-                      <Button className="mb-0 text-sm font-weight-bold">
-                        Visualize 
-                      </Button>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-menu-arrow" right>
-                  <DropdownItem className="noti-title" header tag="div">
-                    <h6 className="text-overflow m-0">Show</h6>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" onClick={()=> {
-                    setValue(0)
-                  }} >
-                    <span 
-                  >Sensor 1</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" onClick={()=> {
-                    setValue(1)
-                  }} >
-                  <span 
-                  >Sensor 2</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile"onClick={()=> {
-                    setValue(2)
-                  }} >
-                  <span 
-                  >Sensor 3</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" onClick={()=> {
-                    setValue(3)
-                  }}>
-                  <span
-                  >Sensor 4</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile"onClick={()=> {
-                    setValue(4)
-                  }} >
-                  <span 
-                  >Sensor 5</span>
-                  </DropdownItem>
-                  <DropdownItem to="/admin/user-profile" >
-                  <DropdownItem to="/admin/user-profile" onClick={()=> {
-                    setValue(-1)
-                  }}>
-                  <span 
-                  >All sensors</span>
-                  </DropdownItem>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-                    
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  {/* Chart */}
-                  
-                  <MainGraph sensorNumber={value} pacient={ props.location.state.pacient} ></MainGraph>
-
-                </CardBody>
-              </Card>
-            </Col>
-            </>
-    }
-
-    
-
-   
-    <Col className="mb-5 mb-xl-0" xl="12">
-    <Card className="shadow">
-    <CardHeader className="border-0">
-    <Row className="align-items-center">
-    <div className="col">
-    <h3 className="mb-0">List of measures</h3>
-    </div>
-    </Row>
-    </CardHeader>
-    <CardBody>
-    <MeasuresWidget pacient = {props.location.state.pacient}> </MeasuresWidget>
-    </CardBody>
-    </Card>
-    </Col>
+    <Col className="mb-5 mb-xl-0" xl="6"></Col>
+    <p>Wrong access, return to the patients list</p> 
     </Row>
     </Container>
-    </>
-    );
-  }
+    
+    </>);
+    return (
+    <>
+    <Header />
+  {/* Page content */}
+  <Container className="mt--7" fluid>
+  <Row className ="text-right">
+  <Col></Col>
+  <Col></Col>
+  <Col>
+  <Anomaly val={info.anomaly}></Anomaly></Col> 
+  <Col className="text-right">
+
+  
+  <Button
+  color="default"
+  onClick={() => sendMail(info.email, infoPodiatrist.email, "Schedule a meeting soon!", emailMessage)}
+  size="sm"
+  >
+  Schedule a meeting 
+  </Button>
+
+  <Button
+  onClick={() => setShow5Graphs(!show5Graphs)}
+  >
+  {(show5Graphs)? "Show main graph": "Show 5 sensors graphs" }
+  </Button>
+  </Col>
+  <Col><Button
+  href="#pablo"
+  onClick={e => e.preventDefault()}
+  >
+  Schedule a meeting
+  </Button></Col>
+  </Row> 
+
+  <Row className="mt-5">
+  
+  {(show5Graphs)?  
+    <>
+    <Col className="mb-5 mb-xl-0" xl="6">
+    <Card className="bg-gradient-default shadow">
+
+    <CardHeader className="bg-transparent">
+
+    <Row className="align-items-center">
+    <div className="col">
+    <h6 className="text-uppercase text-light ls-1 mb-1">
+    Sensor 1
+    </h6>
+    <h2 className="text-white mb-0">Pressure values</h2>
+    </div>
+    <div className="col">
+    </div>
+    </Row>
+
+    </CardHeader>
+
+    <CardBody>
+  {/* Chart */}
+  
+  <Graph sensorNumber={0} pacient={ props.location.state.pacient} ></Graph>
+
+  </CardBody>
+
+  </Card>
+
+  </Col>
+
+  <Col className="mb-5 mb-xl-0" xl="6">
+  <Card className="bg-gradient-default shadow">
+  <CardHeader className="bg-transparent">
+  <Row className="align-items-center">
+  <div className="col">
+  <h6 className="text-uppercase text-light ls-1 mb-1">
+  Sensor 2
+  </h6>
+  <h2 className="text-white mb-0">Pressure values</h2>
+  </div>
+  
+  </Row>
+  </CardHeader>
+  <CardBody>
+{/* Chart */}
+
+<Graph sensorNumber={1} pacient={ props.location.state.pacient} ></Graph>
+
+</CardBody>
+</Card>
+</Col>
+
+<Col className="mb-5 mb-xl-0" xl="6">
+<Card className="bg-gradient-default shadow">
+<CardHeader className="bg-transparent">
+<Row className="align-items-center">
+
+<div className="col">
+<h6 className="text-uppercase text-light ls-1 mb-1">
+Sensor 3
+</h6>
+<h2 className="text-white mb-0">Pressure values</h2>
+</div>
+
+</Row>
+
+</CardHeader>
+
+<CardBody>
+{/* Chart */}
+
+<Graph sensorNumber={2} pacient={ props.location.state.pacient} ></Graph>
+
+</CardBody>
+
+</Card>
+</Col>  
+
+
+<Col className="mb-5 mb-xl-0" xl="6">
+<Card className="bg-gradient-default shadow">
+<CardHeader className="bg-transparent">
+<Row className="align-items-center">
+<div className="col">
+<h6 className="text-uppercase text-light ls-1 mb-1">
+Sensor 4
+</h6>
+<h2 className="text-white mb-0">Pressure values</h2>
+</div>                    
+</Row>
+</CardHeader>
+<CardBody>
+{/* Chart */}
+
+<Graph sensorNumber={3} pacient={ props.location.state.pacient} ></Graph>
+
+</CardBody>
+</Card>
+</Col>
+
+
+<Col className="mb-5 mb-xl-0" xl="6">
+<Card className="bg-gradient-default shadow">
+<CardHeader className="bg-transparent">
+<Row className="align-items-center">
+<div className="col">
+<h6 className="text-uppercase text-light ls-1 mb-1">
+Sensor 5
+</h6>
+<h2 className="text-white mb-0">Pressure values</h2>
+</div>
+</Row>
+</CardHeader>
+<CardBody>
+{/* Chart */}
+
+<Graph sensorNumber={4} pacient={ props.location.state.pacient}></Graph>
+
+</CardBody>
+</Card>
+</Col>
+</> 
+:
+<>
+<Col className="mb-5 mb-xl-0" xl="12">
+<Card className="bg-gradient-default shadow">
+<CardHeader className="bg-transparent">
+<Row className="align-items-center">
+<div className="col">
+<h6 className="text-uppercase text-light ls-1 mb-1">{
+  (value== -1) ? 
+  `All sensors `
+  : `Sensor ${value +1}`
+}                      </h6>
+<h2 className="text-white mb-0">Pressure values</h2>
+</div>
+<UncontrolledDropdown nav>
+<DropdownToggle className="pr-0" nav>
+
+<Button className="mb-0 text-sm font-weight-bold">
+Visualize 
+</Button>
+</DropdownToggle>
+<DropdownMenu className="dropdown-menu-arrow" right>
+<DropdownItem className="noti-title" header tag="div">
+<h6 className="text-overflow m-0">Show</h6>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile" onClick={()=> {
+  setValue(0)
+}} >
+<span 
+>Sensor 1</span>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile" onClick={()=> {
+  setValue(1)
+}} >
+<span 
+>Sensor 2</span>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile"onClick={()=> {
+  setValue(2)
+}} >
+<span 
+>Sensor 3</span>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile" onClick={()=> {
+  setValue(3)
+}}>
+<span
+>Sensor 4</span>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile"onClick={()=> {
+  setValue(4)
+}} >
+<span 
+>Sensor 5</span>
+</DropdownItem>
+<DropdownItem to="/admin/user-profile" >
+<DropdownItem to="/admin/user-profile" onClick={()=> {
+  setValue(-1)
+}}>
+<span 
+>All sensors</span>
+</DropdownItem>
+</DropdownItem>
+</DropdownMenu>
+</UncontrolledDropdown>
+
+</Row>
+</CardHeader>
+<CardBody>
+{/* Chart */}
+
+<MainGraph sensorNumber={value} pacient={ props.location.state.pacient} ></MainGraph>
+
+</CardBody>
+</Card>
+</Col>
+</>
+}
+
+
+
+
+<Col className="mb-5 mb-xl-0" xl="12">
+<Card className="shadow">
+<CardHeader className="border-0">
+<Row className="align-items-center">
+<div className="col">
+<h3 className="mb-0">List of measures</h3>
+</div>
+</Row>
+</CardHeader>
+<CardBody>
+<MeasuresWidget pacient = {props.location.state.pacient}> </MeasuresWidget>
+</CardBody>
+</Card>
+</Col>
+</Row>
+</Container>
+</>
+);
+}
 
